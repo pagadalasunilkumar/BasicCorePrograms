b8257599672b041f0fb52a2e8b32ee2d2e449df2
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-25 09:49:54 +0530

    file is added

---
+++ EvenOrOdd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+            int number;
+
+            // Validate the input
+            while (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Invalid input. Please enter an integer:");
+                input = Console.ReadLine();
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+        }
+    }
+}
