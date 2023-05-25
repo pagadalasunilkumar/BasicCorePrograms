a021b2b98f777826cca151bf5baeb9819d220d6e
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-25 08:08:45 +0530

    [sunil] the file is added

---
+++ RandomFlipCoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class RandomFlipCoin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of times to flip the coin:");
+            string input = Console.ReadLine();
+            int numberOfFlips;
+
+            // Validate the input
+            while (!int.TryParse(input, out numberOfFlips) || numberOfFlips <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer:");
+                input = Console.ReadLine();
+            }
+
+            int headsCount = 0;
+            int tailsCount = 0;
+
+            Random random = new Random();
+
+            // Flip the coin
+            for (int i = 0; i < numberOfFlips; i++)
+            {
+                if (random.NextDouble() < 0.5)
+                    tailsCount++;
+                else
+                    headsCount++;
+            }
+
+            // Calculate the percentages
+            double headsPercentage = (double)headsCount / numberOfFlips * 100;
+            double tailsPercentage = (double)tailsCount / numberOfFlips * 100;
+
+            // Print the results
+            Console.WriteLine($"Heads: {headsCount} ({headsPercentage}%)\nTails: {tailsCount} ({tailsPercentage}%)");
+        }
+    }
+}
